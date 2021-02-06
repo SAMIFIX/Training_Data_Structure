ff17b38bb8191a4d902821fe67a01e1ff5876961
SamiMohsin <admin@graphicalinfo.com>
2021-02-06 19:55:34 +0400

    Create basic_data_structure.py

---
+++ basic_data_structure.py
@@ -0,0 +1,68 @@
+# This is a comment
+# data type
+
+# 1 . number it can be integer or float
+print('\n\n_______________________ الارقام (1) ___________________________\n')
+# integer
+numbers = 1
+
+# print in one line
+# print("number : {0} \nit is type : {1}".format(numbers, type(numbers)))
+
+# simple print
+print("numbers: ", numbers)
+print("numbers type: ", type(numbers))
+
+
+
+
+
+
+print('\n\n_______________________ (2) الأعداد العشرية ___________________________\n')
+# 2. float
+numbers = 1.0
+
+# print in one line
+# print("numbers: {0} \nit is type: {1}".format(numbers, type(numbers)))
+
+# simple print
+print("numbers: ", numbers)
+print("numbers type: ", type(numbers))
+
+
+
+
+print('\n\n_______________________ (3) صح او خطأ ___________________________\n')
+# 3. Boolean or bool is just True or False
+# where True = 1 and False = 0
+numbers = True
+
+# print in one line
+# print("numbers: {0} \nit is type: {1}".format(numbers, type(numbers)))
+print("numbers: ", numbers)
+print("numbers type: ", type(numbers))
+
+
+
+
+
+
+
+print('\n\n_______________________ (4) النصوص، الحروف والكلمات ___________________________\n')
+# 4. String any value or text between "VALUE_HERE"
+numbers = "SAMIFIX"
+# print in one line
+# print("numbers: {0} \nit is type: {1}".format(numbers, type(numbers)))
+print("numbers: ", numbers)
+print("numbers type: ", type(numbers))
+
+
+
+
+
+
+
+
+
+
+
